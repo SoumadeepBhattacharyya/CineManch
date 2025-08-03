6d8b7cae71db695a79d251a2229cfc03222e008b
Soumadeep Bhattacharyya <deepbhattacharyya007@gmail.com>
2025-08-04 00:25:44 +0530

    project completed

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import Movie from "../models/Movie.js";
 import Show from '../models/Show.js';
+import { inngest } from '../inngest/index.js';
 
 
 
@@ -86,6 +87,12 @@ export const addShow = async (req, res) => {
     if (showsToCreate.length > 0) {
       await Show.insertMany(showsToCreate);
     }
+
+    await inngest.send({
+      name:"app/show.added",
+      data:{movieTitle:movie.title}
+
+    })
 
     res.json({ success: true, message: 'Show Added Successfully.' });
   } catch (error) {
